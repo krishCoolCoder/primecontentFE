ed3f417a20e0d7822abbfe81504b34c0933e679b
saikrishnakingtyle <77505364+saikrishnakingtyle@users.noreply.github.com>
2025-05-19 02:02:38 +0530

    feat(contentType): Developed the contentType create operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, CommonModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/create-content-type-page/create-content-type-page.component.ts
+++ src/app/create-content-type-page/create-content-type-page.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-content-type-page',
   standalone: true,
-  imports: [HeaderComponent,SidebarComponent],
+  imports: [HeaderComponent,SidebarComponent, CommonModule, NgFor, FormsModule],
   templateUrl: './create-content-type-page.component.html',
   styleUrl: './create-content-type-page.component.css'
 })
 export class CreateContentTypePageComponent {
-
+  fieldName:any;
+  contentTypeList = [
+    {
+      fieldName: "",
+      fieldType: "String"
+    }
+  ];
+  addAnotherField() {
+    this.contentTypeList.push(
+      {
+        fieldName: "",
+        fieldType: "String"
+      }
+    )
+  };
+  createContentType() {
+    console.log("The contentTypeList is this : ", this.contentTypeList);
+  }
 }
